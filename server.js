c2d1f660eeb504c7497978efec85566595b30eac
saurav7616 <ksaurav7616@gmail.com>
2022-03-17 20:11:55 +0530

    resolving error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-clear-49115',
     user : 'postgres',
     password : 'scott',
     database : 'mak_assignment'
