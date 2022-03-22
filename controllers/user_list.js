4a2cdd8c3dbb9f84859c084f4acdd2326bf89ed7
saurav7616 <ksaurav7616@gmail.com>
2022-03-22 21:51:07 +0530

    handling 0 users

--- controllers/user_list.js
+++ controllers/user_list.js
@@ -1,12 +1,6 @@
 const handleUsers = (res, db) =>{									//returns list of all users
 	db.select('*').from('users')
-	.then(user => {
-		if(user.length){
-			res.json(user)
-		} else {
-			res.status(400).json('userlist not found');
-		}
-	})
+	.then(user => res.json(user))
 	.catch(err => res.status(400).json('error getting users' + err))
 }
 
